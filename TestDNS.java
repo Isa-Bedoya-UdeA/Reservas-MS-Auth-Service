c5a4793566f0249e3a1916f7e789d97d1b997b18
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-04-13 10:41:44 -0500

    Deleted DNS tests

--- TestDNS.java
+++
@@ -1,5 +0,0 @@
-public class TestDNS {
-    public static void main(String[] args) throws Exception {
-        System.out.println(java.net.InetAddress.getByName("aws-1-us-west-2.pooler.supabase.com"));
-    }
-}
